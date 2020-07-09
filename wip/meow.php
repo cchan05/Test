678f824f9e84a917167b25757b35f425e9143a71
cchan05 <cchan05@users.noreply.github.com>
2020-07-10 05:08:23 +0800

    edited

--- wip/meow.php
+++ wip/meow.php
@@ -12,11 +12,7 @@
     </head>
 <body>
 
-<h1>The onclick Event</h1>
-
-<p>The onclick event is used to trigger a function when an element is clicked on.</p>
-
-<p>Click the button to trigger a function that will output "Hello World" in a p element with id="demo".</p>
+<h1>Charts</h1>
 
 <button onclick="piegraph()">Pie</button>
 <button onclick="bargraph()">Bar</button>
